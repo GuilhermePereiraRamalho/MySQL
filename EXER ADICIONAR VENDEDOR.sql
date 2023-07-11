4b5c44884e1d0cc1ffd841e8a359698a51700933
Guilherme Ramalho <137944713+GuilhermePereiraRamalho@users.noreply.github.com>
2023-07-11 11:15:32 -0300

    Add files via upload

--- EXER ADICIONAR VENDEDOR.sql
+++ EXER ADICIONAR VENDEDOR.sql
@@ -1,6 +1,5 @@
 USE sucos;
 
 INSERT INTO tabela_de_vendedores (MATRICULA, NOME, PERCENTUAL_COMISSAO
-) VALUES('00233', 'João Geraldo da Fonseca', 10)
+) VALUES('00233', 'João Geraldo da Fonseca', 0.10)
 
-SELECT * FROM tabela_de_vendedores;
